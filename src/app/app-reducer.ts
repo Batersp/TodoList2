bae452bd105380efe871c1e7dce7050ee00e8025
pavel_ishmukov <p-pup1@mail.ru>
2022-07-13 18:00:34 +0300

    second commit

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,45 +1,33 @@
-export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
-
-const initialState = {
-    status: 'loading' as RequestStatusType,
+const initialState: InitialStateType = {
+    status: 'idle',
     error: null
 }
 
-type InitialStateType = {
-    status: RequestStatusType
-    error: string | null
-}
-
-
-export const appReducer = (state: InitialStateType = initialState, action: AppActionsType): InitialStateType => {
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
-        case 'APP/SET-STATUS': {
-            return {...state, status: action.payload.status}
-        }
-        case "APP/SET-ERROR": {
-            return {...state, error: action.payload.message}
-        }
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
         default:
-            return state
+            return {...state}
     }
 }
 
-export type AppActionsType = SetAppStatusACType
-    | SetAppErrorACType
-
-type SetAppStatusACType = ReturnType<typeof setAppStatusAC>
-type SetAppErrorACType = ReturnType<typeof setAppErrorAC>
-
-export const setAppStatusAC = (status: RequestStatusType) => {
-    return {
-        type: 'APP/SET-STATUS',
-        payload: {status}
-    } as const
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type InitialStateType = {
+    // происходит ли сейчас взаимодействие с сервером
+    status: RequestStatusType
+    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+    error: string | null
 }
 
-export const setAppErrorAC = (message: string | null) => {
-    return {
-        type: 'APP/SET-ERROR',
-        payload: {message}
-    } as const
-}
+export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+
+type ActionsType =
+    | SetAppErrorActionType
+    | SetAppStatusActionType
--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,9 +1,8 @@
-import {setAppErrorAC, setAppStatusAC} from "../app/app-reducer";
-import {ActionsType} from "../features/TodolistsList/tasks-reducer";
-import {Dispatch} from "redux";
-import {ResponseType} from "../api/todolists-api";
+import {setAppErrorAC, SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from '../app/app-reducer'
+import {ResponseType} from '../api/todolists-api'
+import {Dispatch} from 'redux'
 
-export const handleServerError = <T>(data: ResponseType<T>, dispatch: Dispatch<ActionsType>) => {
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
     if (data.messages.length) {
         dispatch(setAppErrorAC(data.messages[0]))
     } else {
@@ -12,8 +11,7 @@ export const handleServerError = <T>(data: ResponseType<T>, dispatch: Dispatch<A
     dispatch(setAppStatusAC('failed'))
 }
 
-export const handleServerNetworkError = (message: string, dispatch:Dispatch<ActionsType>) => {
-    dispatch(setAppErrorAC(message))
+export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
+    dispatch(setAppErrorAC(error.message ? error.message : 'Some error occurred'))
     dispatch(setAppStatusAC('failed'))
 }
-
