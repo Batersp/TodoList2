9ac491017fad42b314eee9508bb6da3663b906a3
pavel_ishmukov <p-pup1@mail.ru>
2022-07-13 21:03:29 +0300

    lesson end

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,6 +1,13 @@
+import {setIsLoggedInAC} from "../features/Login/auth-reducer";
+import {authAPI} from "../api/todolists-api";
+import {Dispatch} from "redux";
+
+const SET_INITIALIZED = 'SET_INITIALIZED'
+
 const initialState: InitialStateType = {
     status: 'idle',
-    error: null
+    error: null,
+    isInitialized: false
 }
 
 export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
@@ -9,6 +16,9 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
             return {...state, status: action.status}
         case 'APP/SET-ERROR':
             return {...state, error: action.error}
+        case SET_INITIALIZED: {
+            return {...state, isInitialized: action.payload.value}
+        }
         default:
             return {...state}
     }
@@ -20,14 +30,34 @@ export type InitialStateType = {
     status: RequestStatusType
     // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
     error: string | null
+    isInitialized: boolean
 }
 
 export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
 export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setInitializedAC = (value: boolean) => {
+    return {
+        type: SET_INITIALIZED,
+        payload: {value}
+    } as const
+}
+
+export const initializeAppTC = () => (dispatch: Dispatch) => {
+    authAPI.me()
+        .then(res => {
+        if (res.data.resultCode === 0) {
+            dispatch(setIsLoggedInAC(true));
+        } else {
+        }
+    })
+        .finally(() => dispatch(setInitializedAC(true)))
+}
 
 export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
 export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetInitializedACType = ReturnType<typeof setInitializedAC>
 
 type ActionsType =
     | SetAppErrorActionType
     | SetAppStatusActionType
+    | SetInitializedACType
